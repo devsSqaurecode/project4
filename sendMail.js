0f2755eedecee1834399e1f40186c50bd499c9e6
Kinjalk <kinjalkbajpai@gmail.com>
2022-11-01 15:18:59 +0530

    Added form validation

--- sendMail.js
+++ sendMail.js
@@ -13,10 +13,11 @@
     let numberOfChildren = document.getElementById("Children").value;
     let comments = document.getElementById("Comments").value;
   
-    if (name === "" || email === "" || phone === "" || country === "" || arrivalDate === "" || departureDate === "" || numberOfAdults === "" || numberOfChildren === "" || comments === "") {
-      window.alert("Fill all the details.");
+    if (name === "" || email === "" || phone === "" || country === "" || arrivalDate === "" || departureDate === "" || numberOfAdults === "none" || numberOfChildren === "none") {
+      console.log("Formed not filled completely. Not sending any mail")
+      window.alert("Please fill the entire form.")
     } else {
-  
+      if(comments === "") comments = "No comments from the sender"
       let contactParams = {
         from_name: name,
         email: email,
@@ -31,5 +32,16 @@
       emailjs
         .send("service_1bpenmo", "template_n36s0z5", contactParams)
         .then(function (res) {});
+        console.log(contactParams)
+        document.getElementById("name").value = ""
+        document.getElementById("email").value = ""
+        document.getElementById("phone").value = ""
+        document.getElementById("Country").value = ""
+        document.getElementById("Arrival").value = ""
+        document.getElementById("Departure").value = ""
+        document.getElementById("Adults").value = ""
+        document.getElementById("Children").value = ""
+        document.getElementById("Comments").value = ""
+    
     }
   }
